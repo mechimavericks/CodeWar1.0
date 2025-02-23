8f15ae611729fd75dc0fef1c3185d7a46fb2774e
Santosh Bhandari <santoshvandari1@gmail.com>
2025-02-23 18:27:05 +0545

    feat: update some data

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
       <Landing />
       <Timeline />
       <Prizes />
-      <Sponsors />
+      {/* <Sponsors /> */}
       <WhatIsHackathon />
       <FAQS />
       <Footer />
