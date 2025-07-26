1b145302d99b587aee5065fccfad022914209bed
sameer-kharel <shameerkharel@gmail.com>
2025-07-26 16:13:37 +0545

    added to vercel~

--- app/page.js
+++ app/page.js
@@ -19,8 +19,8 @@ export default function Home() {
       <Timeline />
       <Prizes />
       <Organized />
-      <Sponsors />
       <EventLeads />
+      <Sponsors />
       <PoweredBy/>
       <SupportedBy />
       <OurAdvisor/>
